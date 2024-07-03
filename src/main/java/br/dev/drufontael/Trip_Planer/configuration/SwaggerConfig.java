ec692110251ff338358049cbf7ba0d8ab5b54506
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2024-07-03 14:17:21 -0300

    :books: docs: Initial swagger configuration.

---
+++ src/main/java/br/dev/drufontael/Trip_Planer/configuration/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package br.dev.drufontael.Trip_Planer.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI docConfig(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Trip-planner API RestFull")
+                        .version("0.0.1-SNAPSHOT")
+                        .description("API for a planner and tracker of travel activities and expenses")
+                );
+    }
+}
