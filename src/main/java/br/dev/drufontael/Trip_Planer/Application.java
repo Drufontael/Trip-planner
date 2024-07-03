2db6aaa8eeefbe0f85db8c7e8ff290bf151529df
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2024-07-03 17:32:37 -0300

    :rocket: deploy: First deployment

--- src/main/java/br/dev/drufontael/Trip_Planer/Application.java
+++ src/main/java/br/dev/drufontael/Trip_Planer/Application.java
@@ -1,8 +1,11 @@
 package br.dev.drufontael.Trip_Planer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
